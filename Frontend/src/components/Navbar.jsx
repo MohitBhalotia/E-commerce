5951a58074df708123c8815ac91093aab82e0767
Mohit Bhalotia <mohitkumarbhalotia420@gmail.com>
2025-01-21 14:21:19 +0530

    Collection Page done

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+  const location = useLocation();
+  const { setShowSearch } = useContext(ShopContext);
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <Link to='/'><img src={assets.logo} className="w-36" alt="Logo" /></Link>
+      <Link to="/">
+        <img src={assets.logo} className="w-36" alt="Logo" />
+      </Link>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
@@ -28,7 +33,13 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} className="w-5 cursor-pointer" />
+        {location.pathname.includes("collection") ? (
+          <img
+            onClick={() => setShowSearch(true)}
+            src={assets.search_icon}
+            className="w-5 cursor-pointer"
+          />
+        ) : null}
         <div className="group relative">
           <img className="w-5 cursor-pointer" src={assets.profile_icon} />
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
